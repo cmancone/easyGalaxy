79c8c3a836d06b61a84fc22d13155234fa5c4bc2
Conor Mancone <cmancone@gmail.com>
2016-12-11 08:35:03 -0500

    Removed unnecessary imports

--- tests/astro_filter/__init__.py
+++
@@ -1,3 +0,0 @@
-from test_filter_properties import test_filter_properties
-from test_solar_vega import test_solar_vega
-from test_calc_mags import test_calc_mags
--- tests/utils/test_to_meters.py
+++ tests/utils/test_to_meters.py
@@ -29,18 +29,16 @@ class test_to_meters(unittest.TestCase):
 		self.assertEqual( utils.to_meters( 1, units='au' ), 1.49598e11 )
 	
 	def test_parsecs( self ):
-		# assertAlmostEqual does not work for large numbers,
-		# so we have to divide by 1e16 to make it work out
+		# assertAlmostEqual has trouble with large numbers
+		# I have to use a negative tolerance or divide
+		# down to small integers.  For no particlar reason
+		# I choose the later.
 		self.assertEqual( utils.to_meters( 1, units='pc' )/1e16, 1.49598e11*3600*180/np.pi/1e16, 7 )
 	
 	def test_kiloparsecs( self ):
-		# assertAlmostEqual does not work for large numbers,
-		# so we have to divide by 1e19 to make it work out
 		self.assertEqual( utils.to_meters( 1, units='kpc' )/1e19, 1.49598e14*3600*180/np.pi/1e19, 7 )
 	
 	def test_megaparsecs( self ):
-		# assertAlmostEqual does not work for large numbers,
-		# so we have to divide by 1e22 to make it work out
 		self.assertAlmostEqual( utils.to_meters( 1, units='mpc' )/1e22, 1.49598e17*3600*180/np.pi/1e22, 7 )
 	
 	# to hertz, to_lambda, and convert_length are all small variations of
