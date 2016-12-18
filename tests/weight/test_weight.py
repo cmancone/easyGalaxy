9aaa820600bdbaafdb99a922c052f7ef535068ab
Conor Mancone <cmancone@gmail.com>
2016-12-18 14:39:37 -0500

    Weight class unit tests

---
+++ tests/weight/test_weight.py
@@ -0,0 +1,40 @@
+import unittest
+import ezgal.weight
+import numpy as np
+
+class test_weight(unittest.TestCase):
+	
+	def test_init( self ):
+		
+		weight = ezgal.weight( 1 )
+		
+		self.assertAlmostEqual( weight.weight, 1, 7 )
+	
+	def test_mult( self ):
+	
+		weight = ezgal.weight( 2 )*ezgal.weight( 3 )
+		
+		self.assertAlmostEqual( weight.weight, 6, 7 )
+	
+	def test_mult_scalar( self ):
+		
+		weight = ezgal.weight( 2 )*3
+		
+		self.assertAlmostEqual( weight.weight, 6, 7 )
+	
+	def test_imult( self ):
+	
+		weight = ezgal.weight( 3 )
+		weight *= ezgal.weight( 4 )
+		
+		self.assertAlmostEqual( weight.weight, 12, 7 )
+	
+	def test_imult_scalar( self ):
+	
+		weight = ezgal.weight( 3 )
+		weight *= 4
+		
+		self.assertAlmostEqual( weight.weight, 12, 7 )
+	
+if __name__ == '__main__':
+	unittest.main()
