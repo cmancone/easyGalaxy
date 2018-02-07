3e90f269634459f30186bd879943b78d227f4bd0
Steven Boada <stevenboada@gmail.com>
2018-02-07 13:12:22 -0500

    install all extra data

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
     name='ezgal',
     packages=['ezgal'],
     package_dir={'ezgal': 'ezgal'},
-    package_data={'ezgal': ['data/refs/*']},
+    package_data={'ezgal': ['data/refs/*', 'data/models/*', 'data/filters/*']},
     version='2.0',
     description='A tool for calculating observables of stellar populations from a grid of SED vs age.',
     author="Conor Mancone",
